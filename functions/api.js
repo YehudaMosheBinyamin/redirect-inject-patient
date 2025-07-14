ed3ef42c8e6fea0b2772a1a42c50625c715d6ba5
YehudaMosheBinyamin <62666416+YehudaMosheBinyamin@users.noreply.github.com>
2025-07-14 15:19:42 +0300

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,1 +1,14 @@
-
+export async function onRequest() {
+  const r = await fetch('https://6xc8wy8oje.execute-api.eu-north-1.amazonaws.com/injectRandomPatient',  {
+    method: 'POST,
+ headers: {
+    'Content-Type': 'application/json'
+},
+ body: JSON.stringify({}) // if your Lambda expects data
+  });
+  const json = await r.json();
+  return new Response(JSON.stringify(json), {
+    headers: { 'content-type': 'application/json',
+               'Access-Control-Allow-Origin': '*' }
+  });
+}
